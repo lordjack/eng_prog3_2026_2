f10ac20d91f8f9a0f5a935ec9fa272242f59d44d
Jackson Meires <lordjackson@gmail.com>
2026-09-03 17:46:24 -0300

    CRUD Usuario BD

--- php/processa.php
+++ php/processa.php
@@ -1,21 +1,10 @@
 <?php
-include 'db.class.php';
 
-//var_dump($_POST);
-//exit;
+$nome = $_GET['nome'];
+$email = $_GET['email'];
 
-if ($_POST['nome'] === '' || $_POST['email'] === '') {
+if ($nome === '' || $email === '') {
     echo 'Nome ou email vazio, por favor preencha os campos!';
 } else {
-    echo "<h3>Bem vindo,". $_POST['nome']." Email: " . $_POST['email'] . '</h3>';
+    echo "<h3>Bem vindo, $nome Email: " . $email . '</h3>';
 }
-
-$db = new db('usuarios');
-
-if (empty($_POST['id'])) {
-     $db->store($_POST);
-} else {
-    $db->update($_POST['id'], $_POST);
-}
-
-header("Location: UsuarioList.php");
